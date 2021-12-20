dda6ddda07ca8e76e9157ccc3b2d42074477c725
Hafezd El Daffa <hafezdeldaffa9@gmail.com>
2021-12-21 02:44:17 +0700

    create signup & login endpoint

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Error Handling Middleware
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+// Middleware to setHeader
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
--- controllers/keluarga.js
+++ controllers/keluarga.js
@@ -1,3 +1,5 @@
 /* 
     CONTROLLER UNTUK KELUARGA
 */
+
+const Keluarga = require('../models/keluarga');
