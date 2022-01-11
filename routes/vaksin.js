5e045db7774993fea073daba81e0f7dacce471c1
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2022-01-11 22:34:26 +0700

    add route vaksin by ID

--- routes/vaksin.js
+++ routes/vaksin.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateJWT } = require('../controllers/auth');
-const { getVaksin, getVaksinRT } = require('../controllers/vaksin');
+const { getVaksin, getVaksinRT, getVakinByID } = require('../controllers/vaksin');
 
 router.get('/vaksin', authenticateJWT, getVaksin);
+
+router.get('/vaksin/:id', authenticateJWT, getVakinByID);
 
 router.get('/vaksin-rt', authenticateJWT, getVaksinRT);
 
